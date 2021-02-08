435cb3651e4487a36f200b54cce5ba0d74870d6e
Dillon Draper <dillonbdraper@gmail.com>
2021-02-08 13:52:14 -0600

    Successfully implemented viewset/url handling for events in the server.  Still many questions, especially about serialization, but less overwhelmed.

--- levelupapi/models/events.py
+++ levelupapi/models/events.py
@@ -1,11 +1,12 @@
 from levelupapi.models.games import Game
 from levelupapi.models.gamer import Gamer
 from django.db import models
-from django.db.models.fields import CharField, DateTimeField
+from django.db.models.fields import CharField, DateField, TimeField
 from django.db.models.fields.related import ForeignKey
 
 class Event(models.Model):
-    event_time = DateTimeField()
+    organizer = models.ForeignKey(Gamer, on_delete=models.CASCADE)
+    description = CharField(max_length=200)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    location = CharField(max_length=100)
-    scheduler = models.ForeignKey(Gamer, on_delete=models.CASCADE)
+    date = DateField(default= '2021-02-04')
+    time = TimeField(default= '12:00')
